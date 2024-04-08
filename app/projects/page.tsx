3347418243d6b77a1e425d80e3697e7ede297ffb
thirumurugan sivalingam <thiru@synlian.net>
2024-04-08 21:10:50 +0530

    changed tags

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -57,14 +57,14 @@ function Page() {
           <div className='absolute inset-0 w-[100vw] flex justify-center items-center'>
             <div className='bg-[rgba(0,0,0,0.7)]/70 z-[9999] absolute backdrop-blur-md inset-0'></div>
             <div className='h-[500px] w-[450px] z-[99999] bg-[#fff1e3] rounded-md p-5'>
-              <h2 className='text-2xl font-semibold text-black mb-4 text-center '>Policy Creation</h2>
+              <h2 className='text-2xl font-semibold text-black mb-4 text-center '>Project Registation</h2>
               <form onSubmit={handleSubmit} className='px-8 py-6'>
                 <input type='text' name='Project' value={formData.name} onChange={handleInputChange} placeholder='Name' className='w-full border-b focus:border-none rounded-md bg-transparent px-3 py-2 mb-3' />
                 <input type='text' name='location' value={formData.location} onChange={handleInputChange} placeholder='Location' className='w-full border-b rounded-md bg-transparent px-3 py-2 mb-3' />
                 <input type='date' name='Started at' value={formData.proofOfWork} onChange={handleInputChange} placeholder='' className='w-full border-b rounded-md bg-transparent px-3 py-2 mb-3' />
           
                 <input type='text' name='Offsetted Carbon' value={formData.numberValue} onChange={handleInputChange} placeholder='Number Value' className='w-full border-b rounded-md bg-transparent px-3 py-2 mb-3' />
-                <button type='submit' className='bg-[#6BA865] text-white font-bold px-4 py-2 rounded-md w-full mt-5'>Create a Policy</button>
+                <button type='submit' className='bg-[#6BA865] text-white font-bold px-4 py-2 rounded-md w-full mt-5'>Register</button>
               </form>
             </div>
           </div>
